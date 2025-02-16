386ed412c49f756261aa074acafae5b48830a9ba
kip Emmanuel <emmanuel.kipngetich@cellulant.io>
2025-02-16 17:09:14 +0300

    Added a detailed readme  for easy cloning and using the app locally

--- crew/environment.py
+++
@@ -1,9 +0,0 @@
-import os
-from utils.helpers import get_openai_api_key, get_serper_api_key
-
-class CrewEnvironment:
-    """Configures API keys and environment variables."""
-    @staticmethod
-    def configure():
-        os.environ["OPENAI_MODEL_NAME"] = 'gpt-3.5-turbo'
-        os.environ["SERPER_API_KEY"] = get_serper_api_key()
